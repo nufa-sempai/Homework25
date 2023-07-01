3736861ae38b7aff334a6be7af481ee53289a3b7
nufa_sempai <nufa_sempai@mail.ru>
2023-07-01 19:20:37 +0300

    ДЗ 25 - Тестирование

---
+++ src/test/java/com/example/homework25/Homework25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
